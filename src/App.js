597194dff2f8aa70dade36c8fd5b3015270be25e
CelineMsn <92699111+CelineMsn@users.noreply.github.com>
2022-09-08 15:33:22 -0700

    wordplay media update

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
             alt="logo dictionary app celine"
           />
         </header>
-        <main className="pb-2">
+        <main className="pb-1">
           <Dictionary />
           <Footer />
         </main>
